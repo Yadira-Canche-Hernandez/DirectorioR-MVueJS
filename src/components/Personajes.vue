2eb1340356b1b5b8d009722f758adbb7d1750949
Yadira-Canche-Hernandez <yadira.19070010@itsmotul.edu.mx>
2023-02-25 00:18:43 -0600

    Buscador modificado como función InfoPer-ya muestra de todas las paginas pero por nombre

--- src/components/Personajes.vue
+++ src/components/Personajes.vue
@@ -8,26 +8,38 @@ export default {
 
   data() {
     return {
+      //variables de retorno  
+      //para lista de personajes
       info: [],
       personajes: [],
       cont:2,
-      search:"",
+
+      //para tarjeta por personaje desde lista
       id: 0,
       infoUno:[],
       personaje: [],
       mostrar: false,
+
+      //para buscador
+      buscar:"",
+      buscados: [],
+      mostrarBuscados: false,
+
     }
   },
 
   mounted() {
     axios.get(API_URL)
       .then((response) => {
+        //paginas
         this.info = response.data.info;
+        //resultados
         this.personajes = response.data.results;
       })
   },
 
   methods: {
+
     //metodo para pagina anterior
     pagRe(num) {
       API_URL='https://rickandmortyapi.com/api/character/?page='+(num-1)
@@ -44,6 +56,7 @@ export default {
       
       
     },
+
     //metodo para pagina siguiente
     pagSig(num) {
       //url de consumo API
@@ -58,35 +71,42 @@ export default {
       })
       this.cont++
     },
+
     //informacion de un personaje
-    InfoPer(id) {
-      
+    InfoPer(id) {      
       //url de consumo API
       API_URL='https://rickandmortyapi.com/api/character/'+id 
       console.log(API_URL)
       //lo obtiene
       axios.get(API_URL)
-      //
+      //tomamos la respuesta
       .then((response) => {
         console.log(response) //regresa datos de un solo personaje
         //contiene el array de cada personaje con sus datos
         this.infoUno = response.data;
         console.log(this.infoUno)
       })
+      //el id incrementa
       this.id++
+      //cambiamos el valor por verdadero para que lo muestre
       this.mostrar=true
       console.log(this.mostrar)
-    }
-  },
-  computed: {
-    //metodo para buscar
-    filtrarBusqueda(){
-      //busca en cada personaje
-      return this.personajes.filter((personaje) => {
-        console.log(personaje)
-        //busca en el nombre del personaje
-        return personaje.name.toLowerCase().includes(this.search.toLowerCase())
-      })
+    },
+    //funcion para buscar personajes
+    buscador(buscar)  { 
+      
+      //lo busca por nombre                
+      API_URL='https://rickandmortyapi.com/api/character/?name='+buscar
+      console.log(API_URL)
+
+      axios.get(API_URL)
+        .then((response) => {
+          //toma todos los personajes que encuentra en resultados y lo almacena en la variable buscados
+          this.buscados = response.data.results;
+          console.log(this.buscados)
+        })
+        //cambiamos a verdadero
+        this.mostrarBuscados=true
     },
   },
 }
@@ -98,24 +118,16 @@ export default {
   <!--Total de personajes-->
   <h2 class="text-2xl my-10 text-center mx-5 sm:py-2">Hay un total de {{ info.count }} personajes en el programa de Rick & Morty</h2>
 
-  <!--buscador por nombre por pagina-->
-  <div class="flex justify-align-center mx-auto  my-5 ">
-      
-    <h3 class="text-2xl text-center my-3 mx-5 sm:py-2">Buscar</h3>
-    <input type="text" v-model="search" placeholder="Buscar por nombre">
-  </div>
-
-<!--busqueda de personajes-->
-  <div class="mx-auto">
-    <div class="content-personajes flex" v-for="personaje in filtrarBusqueda"  :key="personaje.id">
-
-      <div>
-        {{ personaje.name }}
-      </div>
-
+  <!--Buscador-->
+    <div class="flex justify-center items-center space-x-4 text-base my-8">
+      <input class="h-12 wl-5 px-7" type="text" v-model="buscar" placeholder="Buscar por nombre o id">
+      <button
+        @click="buscador(buscar)" 
+        class="h-10 wl-5 px-7 font-semibold rounded-md bg-black text-white justify-center shadow-xl hover:shadow-inner transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105" type="center">
+        Buscar
+      </button>
     </div>
-  </div>
-    
+
   <!--Botones paginas-->
   <div class="mt-5">
       <!--Pagina anterior-->
@@ -133,8 +145,7 @@ export default {
         <ul class="text-xl">
           <li v-for="p in personajes">
             <!--Llamando función obtener info personal por personaje-->
-            <button @click="InfoPer(p.id)">{{ p.name }} id:{{ p.id }}</button>
-            
+            <button @click="InfoPer(p.id)">{{ p.name }} id:{{ p.id }}</button> 
           </li>
         </ul>
       </div>
